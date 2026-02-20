6eca242f2fbdee60eecb2e22f8e749512b26755c
Pete Blackburn <p.d.blackburn0@gmail.com>
2026-02-20 15:11:31 +0000

    Fix login functionality and form POST action

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login
 from allauth.account.forms import SignupForm
 
 
@@ -9,6 +10,19 @@ def index(request):
 
 
 def login(request):
+    """Handle user login"""
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            messages.success(request, f'Welcome back, {user.username}!')
+            return redirect('hello_world:index')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    
     return render(request, 'login.html')
 
 
